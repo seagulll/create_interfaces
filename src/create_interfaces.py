50a6cdb6609821d118eb8221ef8c4eed10203247
seagulll <ling.d.yu@gmail.com>
2014-11-26 16:28:05 +0800

    增加功能： 1. IPv4第3段地址的处理。 2。 结果写入文件。
    Ling
    11/26/2014

--- src/create_interfaces.py
+++ src/create_interfaces.py
@@ -11,7 +11,7 @@ def parse_args():
 this is the Ubuntu interfaces file generator.
 Run it like this:
 
-    python create_interfaces.py -d eth1 -n 10.200.1 -s 1 -e 20 -x 1 -m 255.255.0.0
+    python create_interfaces.py -d eth1 -n 10.29 -t 101 -p 124 -s 1 -e 254 -x 1 -m 255.255.0.0 -o "C:\simon\workspace\create_interfaces\src" -f "int.txt"
     
 it means it  will generate network configurations for Ubuntu OS, like:
 
@@ -33,28 +33,38 @@ broadcast 10.200.1.255
 
     parser = optparse.OptionParser(usage)
     parser.add_option("-d", "--device", dest="device", help="please input device", default="eth1")
-    parser.add_option("-n", "--netaddress", dest="netaddress", help="please input netaddress", default="10.200.1")
-    parser.add_option("-s", "--startaddress", dest="startaddress", type="int", help="please input startaddress", default=1)
-    parser.add_option("-e", "--stopaddress", dest="stopaddress", type="int", help="please input stopaddress", default=20)
+    parser.add_option("-n", "--netaddress", dest="netaddress", help="please input netaddress", default="10.200")
+    parser.add_option("-t", "--startaddress_1", dest="startaddress_1", type="int", help="please input startaddress_1", default=1)
+    parser.add_option("-p", "--stopaddress_1", dest="stopaddress_1", type="int", help="please input stopaddress_1", default=20)
+    parser.add_option("-s", "--startaddress_2", dest="startaddress_2", type="int", help="please input startaddress_2", default=1)
+    parser.add_option("-e", "--stopaddress_2", dest="stopaddress_2", type="int", help="please input stopaddress_2", default=20)
     parser.add_option("-x", "--startindex", dest="startindex", type="int", help="please input startindex", default=1)
     parser.add_option("-m", "--networkmask", dest="networkmask", help="please input networkmask", default="255.255.0.0")
+    parser.add_option("-o", "--directory", dest="directory", type="string", help="please input the file's directory", default="c:")
+    parser.add_option("-f", "--file", dest="file", type="string", help="please input the file's name", default="int.txt")
     options, _ = parser.parse_args()
     
-    return options.device, options.netaddress, options.startaddress, options.stopaddress, options.startindex, options.networkmask
+    return options.device, options.netaddress, options.startaddress_1, options.stopaddress_1, options.startaddress_2, options.stopaddress_2, options.startindex, options.networkmask, options.directory, options.file
 
 def create_interfaces():
-    dev, netadd, startadd, stopadd, startindx, netmask = parse_args()
-    conf = ''
-    for i in range(startadd, stopadd + 1):
-        conf = conf + 'auto ' + dev + ':' + str(startindx) + '\n'
-        conf = conf + 'iface ' + dev + ':' + str(startindx) + ' inet static\n' 
-        conf = conf + 'address ' + netadd + '.' + str(i) + '\n'
-        conf = conf + 'netmask ' + netmask + '\n'
-        conf = conf + 'broadcast ' + netadd + '.255\n'
-        conf = conf + '\n'
-        startindx = startindx +1
-        
-    print conf
+    dev, netadd, startadd_1, stopadd_1, startadd_2, stopadd_2, startindx, netmask, directory, file_name = parse_args()
+    int_file = open(directory + "//" + file_name, 'a')
+
+    for j in range(startadd_1, stopadd_1 + 1):
+        for i in range(startadd_2, stopadd_2 + 1):
+            conf = ''
+            conf = conf + 'auto ' + dev + ':' + str(startindx) + '\n'
+            conf = conf + 'iface ' + dev + ':' + str(startindx) + ' inet static\n' 
+            conf = conf + 'address ' + netadd + '.' + str(j)
+            conf = conf + '.' + str(i) + '\n'
+            conf = conf + 'netmask ' + netmask + '\n'
+            conf = conf + 'broadcast ' + netadd + '.' + str(j)
+            conf = conf + '.255\n'
+            conf = conf + '\n'
+            startindx = startindx + 1
+            print conf
+            int_file.writelines(conf)
+    int_file.close()
     
 if __name__ == '__main__':
     create_interfaces()
